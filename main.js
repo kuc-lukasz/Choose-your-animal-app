7413e95365216232615e8267e5867b9edc488c70
lukaq <82107173+kuc-lukasz@users.noreply.github.com>
2021-10-06 18:49:28 +0200

    add JS

--- main.js
+++ main.js
@@ -3,12 +3,39 @@ const animalsWindow = document.querySelector(".animals")
 const animal = document.querySelector('.animal')
 const addBtn = document.querySelector(".btn-add")
 
+// animalsWindow.addEventListener('click', (e) =>{
+// console.log(e)
+// const newFavAnimal = e.target.textContent
+// favAnimal.innerText = newFavAnimal
+// })
+
+// addBtn.addEventListener('click', () => {
+//     const newAnimal = prompt('Write animal');
+//     if(newAnimal === null || newAnimal.trim() === "" ){
+//         return 
+//     } else {
+//     const newAnimalBox = document.createElement('div')
+//     newAnimalBox.textContent = newAnimal
+//     newAnimalBox.classList.add('animal')
+//     animalsWindow.appendChild(newAnimalBox)
+//     }
+
+// })
+
+
+animalsWindow.addEventListener("click", function (e) {
+console.log(e)
+const favAnOnTheTop = e.target.textContent
+favAnimal.innerText = favAnOnTheTop
+})
+
 addBtn.addEventListener('click', () => {
-    const newAnimal = prompt('Write animal');
-    const newAnimalBox = document.createElement('div')
-    newAnimalBox.textContent = newAnimal
-    newAnimalBox.classList.add('animal')
-    animalsWindow.appendChild(newAnimalBox)
-    
+const newAnimal = prompt('Wpisz zwierze')
+const newBox = document.createElement('div')
+newBox.classList.add('animal')
+newBox.innerText = newAnimal
+animalsWindow.appendChild(newBox)
+
+
 
 })
